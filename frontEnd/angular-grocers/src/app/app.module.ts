6fbbad8c913887121dbe566dfadbb20b213ccb54
Mike Batani <mkbatani@gmail.com>
2021-04-25 11:12:20 -0700

    replaced product.server.ts add formmodule and httpclient on app.module

--- frontEnd/angular-grocers/src/app/app.module.ts
+++ frontEnd/angular-grocers/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SendRequestComponent } from './send-request/send-request.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SendRequestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontEnd/angular-grocers/src/app/product.service.ts
+++ frontEnd/angular-grocers/src/app/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class Grocer.ServiceService {
+export class ProductService {
 
   constructor() { }
 }
