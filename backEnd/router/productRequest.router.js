2332e20f691e38bc8a5ab222623b15fec23fe485
Avichel Verma <averma18@hawk.iit.edu>
2021-04-26 12:00:23 -0500

    CRUD operations for product

--- backEnd/router/productRequest.router.js
+++ backEnd/router/productRequest.router.js
@@ -7,9 +7,7 @@ let productRequest = require("../controller/productRequest.controller.js");
 
 //If the path is /product/... than it will match to the subpath 
 
-router.post("/requestForm", productRequest.createRequestForm);
-router.get("/retrieveReqForm", productRequest.getAllFormsRequests);
-router.delete("/removeform/:pid", productRequest.removeFormReqByName);
+router.post("/productRequest", productRequest.createRequestForm);
 //router.post("/insert", productRequest.createRequestForm);
 
 module.exports = router;
