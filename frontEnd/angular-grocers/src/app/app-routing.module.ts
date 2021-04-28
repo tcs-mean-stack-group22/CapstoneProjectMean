a1761c7549a2153a5647852215c2b4c40930ee34
Harshith Gudapati <jinnu95@gmail.com>
2021-04-28 11:36:42 -0500

    Edited lock-user  component

--- frontEnd/angular-grocers/src/app/app-routing.module.ts
+++ frontEnd/angular-grocers/src/app/app-routing.module.ts
@@ -3,14 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { LockUserComponent } from './lock-user/lock-user.component';
+import { OrderStatusComponent } from './order-status/order-status.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { UpdateProductComponent } from './update-product/update-product.component';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
 	{ path: 'signup', component: SignupComponent },
-	{ path: 'admin-panel', component: AdminPanelComponent },
-	{ path: 'update-product', component: UpdateProductComponent },
-	{ path: '', redirectTo: 'admin-panel', pathMatch: 'full' }
+	{ path: 'adminpanel', component: AdminPanelComponent },
+	{ path: 'lockuser', component: LockUserComponent },
+	{ path: 'orderstatus', component: OrderStatusComponent },
+	{ path: 'editprofile', component: EditProfileComponent },
+	{ path: 'updateproduct', component: UpdateProductComponent },
+	{ path: '', redirectTo: 'adminpanel', pathMatch: 'full' }
 ];
 
 @NgModule({
