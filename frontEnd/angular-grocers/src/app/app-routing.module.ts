1b2ce66e4e7c6a71f3a0c77d8bf18c8e13af20f4
Noah Zbinden <manoahz@gmail.com>
2021-04-28 22:15:44 -0500

    navbar Changes

--- frontEnd/angular-grocers/src/app/app-routing.module.ts
+++ frontEnd/angular-grocers/src/app/app-routing.module.ts
@@ -8,6 +8,12 @@ import { OrderStatusComponent } from './order-status/order-status.component';
 import { SendRequestComponent } from './send-request/send-request.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewProductsComponent} from './view-products/view-products.component'
+import { ViewRequestsComponent} from './view-requests/view-requests.component'
+import { AddProductCartComponent} from './add-product-cart/add-product-cart.component'
+import { ViewProductCartComponent} from './view-product-cart/view-product-cart.component'
+import { CheckoutComponent} from './checkout/checkout.component';
+import { EditProfileUserComponent} from './edit-profile-user/edit-profile-user.component';
 
 import { UserPanelComponent} from './user-panel/user-panel.component';
 import { RaiseTicketComponent} from './raise-ticket/raise-ticket.component';
@@ -19,10 +25,22 @@ const routes: Routes = [
 	{ path: 'signup', component: SignupComponent },
 	{ path: 'adminpanel', component: AdminPanelComponent },
 	{ path: 'lockuser', component: LockUserComponent },
-	{ path: 'orderstatus', component: OrderStatusComponent },
+	
 	{ path: 'sendrequest', component: SendRequestComponent },
 	{ path: 'editprofile', component: EditProfileComponent },
 	{ path: 'updateproduct', component: UpdateProductComponent },
+
+	//Admin Paths
+	{ path: 'viewproduct', component: ViewProductsComponent},
+	{ path: 'viewrequest', component: ViewRequestsComponent},
+
+	//User Only Paths 
+	{ path: 'addtocart', component: AddProductCartComponent},
+	{ path: 'viewcart', component: ViewProductCartComponent},
+	{ path: 'checkout', component: CheckoutComponent},
+	{ path: 'orderstatus', component: OrderStatusComponent },
+	{ path: 'editprofileuser', component: EditProfileComponent},
+	{ path: 'funds', component: FundsComponent },
 
 	{ path: 'sentrequest', component: SendRequestComponent },
 	{ path: 'userpanel', component: UserPanelComponent },
